a1a26172bf4a1b0fb701ce199282fd11d0eea159
Sneh674 <naiksneh6@gmail.com>
2024-09-30 20:15:55 +0530

    vh1.01

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
 import { useState } from 'react'
 import './App.css'
-import { createBrowserRouter, RouterProvider, HashRouter, Link } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Contact from './component/Contact'
 import Home from './component/Home'
 import About from './component/About'
 import Navbar from './component/Navbar'
 import Gallery from './component/Gallery'
-// import { Link }
-
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <><Navbar/><Home /></>,
-      errorElement: <div>errorrrrr</div>
-    },
-    {
-      path: "/about",
-      element: <><Navbar/><About /></>
-    },
-    {
-      path: "/contact",
-      element: <><Navbar/><Contact /></>
-    },
-    {
-      path:"/gallery",
-      element:<><Navbar/><Gallery/></>
-    }
-  ])
-
   return (
-    <>
-
-      <RouterProvider router={router} />
-    </>
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="*" element={<div>Error: Page not found</div>} />
+      </Routes>
+    </HashRouter>
   )
 }
 
---
+++ src/App2.jsx
@@ -0,0 +1,41 @@
+// import { useState } from 'react'
+// import './App.css'
+// import { createBrowserRouter, RouterProvider, HashRouter, Link } from 'react-router-dom'
+// import Contact from './component/Contact'
+// import Home from './component/Home'
+// import About from './component/About'
+// import Navbar from './component/Navbar'
+// import Gallery from './component/Gallery'
+// // import { Link }
+
+
+// function App() {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <><Navbar/><Home /></>,
+//       errorElement: <div>errorrrrr</div>
+//     },
+//     {
+//       path: "/about",
+//       element: <><Navbar/><About /></>
+//     },
+//     {
+//       path: "/contact",
+//       element: <><Navbar/><Contact /></>
+//     },
+//     {
+//       path:"/gallery",
+//       element:<><Navbar/><Gallery/></>
+//     }
+//   ])
+
+//   return (
+//     <>
+
+//       <RouterProvider router={router} />
+//     </>
+//   )
+// }
+
+// export default App
